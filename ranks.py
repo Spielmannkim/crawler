67939efd28528eb62482ae2b40d9a08b4870f3bf
shinjong kim <63417540+Spielmannkim@users.noreply.github.com>
2023-04-07 15:46:30 +0900

    매일 각 날짜 폴더마다 개별적으로 판매수모음 엑셀파일 생성

--- ranks.py
+++ ranks.py
@@ -1,53 +1,59 @@
 import os
 import pandas as pd
 
-# 파일 경로 설정
-folder_path = './'
+# 현재 날짜 가져오기
+today = pd.Timestamp.now().strftime("%Y%m%d")
 
-# 파일들의 경로 및 이름 설정
-file_paths = [os.path.join(folder_path, file_name) for file_name in os.listdir(folder_path) if 'gmarket' in file_name and file_name.endswith('.xlsx')]
+# 현재 날짜 이름을 가진 하위폴더들의 경로 설정
+folder_paths = [os.path.join('.', folder_name) for folder_name in os.listdir('.') if folder_name.startswith('gmarket') and folder_name.endswith(today)]
 
-# 데이터를 담을 빈 딕셔너리 생성
-data_dict = {}
+# 각 하위폴더마다 데이터 추출 후 새로운 엑셀파일 생성
+for folder_path in folder_paths:
+    # 파일들의 경로 및 이름 설정
+    file_paths = [os.path.join(folder_path, file_name) for file_name in os.listdir(folder_path) if file_name.endswith('.xlsx') and 'gmarket' in file_name and today in file_name]
 
-# 파일들을 순회하며 데이터 추출
-for file_path in file_paths:
-    # 엑셀 파일 읽어오기
-    df = pd.read_excel(file_path)
+    # 데이터를 담을 빈 딕셔너리 생성
+    data_dict = {}
 
-    # 각 상품에 대해 데이터 추출
-    for i in range(len(df)):
-        product = df.loc[i, '상품명']
-        link = df.loc[i, '링크']
-        price = df.loc[i, '가격']
-        ranks = df.loc[i, '순위']
+    # 파일들을 순회하며 데이터 추출
+    for file_path in file_paths:
+        # 엑셀 파일 읽어오기
+        df = pd.read_excel(file_path)
 
-        # 해당 상품명이 이미 딕셔너리에 있는 경우, 해당 상품명의 리스트에 데이터 추가
-        if product in data_dict:
-            data_dict[product]['링크'].append(link)
-            data_dict[product]['가격'].append(price)
-            data_dict[product]['순위'].append(ranks)
-        # 해당 상품명이 딕셔너리에 없는 경우, 새로운 딕셔너리 생성 후 데이터 추가
-        else:
-            data_dict[product] = {
-                '링크': [link],
-                '가격': [price],
-                '순위': [ranks]
-            }
+        # 각 상품에 대해 데이터 추출
+        for i in range(len(df)):
+            product = df.loc[i, '상품명']
+            link = df.loc[i, '링크']
+            price = df.loc[i, '가격']
+            ranks = df.loc[i, '순위']
 
-# 데이터를 담을 빈 데이터프레임 생성
-result_df = pd.DataFrame(columns=['상품명', '링크', '가격', '순위1', '순위2', '순위3'])
+            # 해당 상품명이 이미 딕셔너리에 있는 경우, 해당 상품명의 리스트에 데이터 추가
+            if product in data_dict:
+                data_dict[product]['링크'].append(link)
+                data_dict[product]['가격'].append(price)
+                data_dict[product]['순위'].append(ranks)
+            # 해당 상품명이 딕셔너리에 없는 경우, 새로운 딕셔너리 생성 후 데이터 추가
+            else:
+                data_dict[product] = {
+                    '링크': [link],
+                    '가격': [price],
+                    '순위': [ranks]
+                }
 
-# 딕셔너리의 데이터를 데이터프레임에 추가
-for i, (product, data_dict) in enumerate(data_dict.items()):
-    # 상품명, 링크, 가격 추가
-    result_df.loc[i, '상품명'] = product
-    result_df.loc[i, '링크'] = data_dict['링크'][0]
-    result_df.loc[i, '가격'] = data_dict['가격'][0]
+    # 데이터를 담을 빈 데이터프레임 생성
+    result_df = pd.DataFrame(columns=['상품명', '링크', '가격', '순위1', '순위2', '순위3'])
 
-    # 순위 데이터 추가
-    for j, ranks in enumerate(data_dict['순위']):
-        result_df.loc[i, f'순위{j+1}'] = ranks
+    # 딕셔너리의 데이터를 데이터프레임에 추가
+    for i, (product, data_dict) in enumerate(data_dict.items()):
+        # 상품명, 링크, 가격 추가
+        result_df.loc[i, '상품명'] = product
+        result_df.loc[i, '링크'] = data_dict['링크'][0]
+        result_df.loc[i, '가격'] = data_dict['가격'][0]
 
-# 결과 데이터프레임을 엑셀 파일로 저장
-result_df.to_excel('순위모음.xlsx', index=False)
+        # 순위 데이터 추가
+        for j, ranks in enumerate(data_dict['순위']):
+            result_df.loc[i, f'순위{j+1}'] = ranks
+
+    # 결과 데이터프레임을 엑셀 파일로 저장
+    result_df.to_excel(os.path.join(folder_path, f'순위모음_{today}.xlsx'), index=False)
+    print(f"순위모음_{today}.xlsx이라는 이름으로 저장되었습니다 =)")
