3926ef86b12814ba7a7a24455470043a8cea4272
shinjong kim <63417540+Spielmannkim@users.noreply.github.com>
2023-04-07 15:28:44 +0900

    날짜별 폴더 생성 후 해당 폴더에 엑셀파일 생성

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pandas as pd
 from lxml import html
@@ -8,9 +9,17 @@ session = requests.Session()
 while True:
     # 현재 시간 가져오기
     current_time = time.strftime('%Y%m%d_%H%M')
+    current_date = time.strftime('%Y%m%d')
     
+    # 폴더 이름 설정
+    folder_name = f'gmarket_{current_date}'
+    
+    # 폴더가 존재하지 않으면 폴더 생성하기
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
     # 파일 이름 설정
-    file_name = 'gmarket_' + current_time + '.xlsx'
+    file_name = f'{folder_name}/gmarket_{current_time}.xlsx'
 
     # URL 설정
     url = "https://corners.gmarket.co.kr/superdeals"
@@ -24,7 +33,7 @@ while True:
     data_name = []
     data_link = []
     data_price = []
-    for i in range(1, 301):
+    for i in range(1, 300):
         fullxpath_quantity = f"/html/body/div[3]/div/div[2]/ul/li[{i}]/div/div/span[3]/span/strong"
         fullxpath_name = f"/html/body/div[3]/div/div[2]/ul/li[{i}]/div/a/span"
         fullxpath_link = f"/html/body/div[3]/div/div[2]/ul/li[{i}]/div/a/@href"
@@ -56,11 +65,11 @@ while True:
             break
 
     # 추출한 데이터를 이용해 pandas 데이터프레임 생성하기
-    df = pd.DataFrame({'순위': range(1, len(data_name)+1),
-                    '상품명': data_name,
-                    '판매수': data_quantity,
-                    '링크': data_link,
-                    '가격': data_price})
+    df = pd.DataFrame({'순위': [i for i in range(1, len(data_name)+1)],
+                '상품명': data_name,
+                '판매수': data_quantity,
+                '링크': data_link,
+                '가격': data_price})
 
     # 데이터프레임을 숫자 형식으로 엑셀 파일로 저장하기
     with pd.ExcelWriter(file_name) as writer:
@@ -68,5 +77,5 @@ while True:
 
     print(f"{file_name} 이라는 이름으로 저장되었습니다 =)")
 
-    # 다시 반복문을 실행하기 전 1분간 대기하기
-    time.sleep(60)
+    # 다시 반복문을 실행하기 전 5분간 대기하기
+    time.sleep(300)
